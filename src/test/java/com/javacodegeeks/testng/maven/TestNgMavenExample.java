4cc4c1267c374b26dcd20d60dd93c7735a23c2a0
Abhishek kumar <abhishek7969@gmail.com>
2017-02-07 13:59:49 +0530

    Update TestNgMavenExample.java

--- src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
@@ -9,6 +9,6 @@ public class TestNgMavenExample {
 	@Test
 	public void exampleOfTestNgMaven() {
 		System.out.println("This is TestNG-Maven Example");
-			Assert.assertEquals("Google", "Google12");
+			Assert.assertEquals("Google", "Google");
 	}
 }
