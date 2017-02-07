66217d45b55ab0e8096c64102b9fe65e2dcfe115
Abhishek kumar <abhishek7969@gmail.com>
2017-02-07 13:57:41 +0530

    Update TestNgMavenExample.java

--- src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
@@ -1,11 +1,14 @@
 package com.javacodegeeks.testng.maven;
 
 import org.testng.annotations.Test;
+import org.testng.Assert;
+
 
 public class TestNgMavenExample {
 
 	@Test
 	public void exampleOfTestNgMaven() {
 		System.out.println("This is TestNG-Maven Example");
+			Assert.assertEquals("Google", "Google");
 	}
 }
